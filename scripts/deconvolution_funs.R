ee2e73aaaa09b1db40ce86617b2e1170e3bea425
jonasfreimuth <68993915+jonasfreimuth@users.noreply.github.com>
2022-06-05 16:56:21 +0200

    simplify get_protein_mut
      introduce a rather large TODO: resolve deletions business
      and adapt variable naming

--- scripts/deconvolution_funs.R
+++ scripts/deconvolution_funs.R
@@ -22,6 +22,7 @@ parse_snv_csv <- function(snvfile, ...) { # allele frequency from v-pipe vcf
 }
 
 detectable_deletions <- function(x, colnames) {
+  # TODO Currently unused, see note on deletions in get_protein_mut
   #' deletions can span a range of position values indicated by a dash in the
   #' prot_mut_loc column. In those cases those ranges have to be split up in
   #' order to be able to detect single positions. This function should be used
@@ -112,74 +113,59 @@ get_protein_mut <- function(vepfile) {
   vepfile_df <- read.csv(vepfile, sep = ",", header = TRUE)
   # parsing snv and protmut location
 
-  # parsing gene mutation
-  gene_mutation <- data.frame(
-    gene_mut_loc = str_split_fixed(vepfile_df$Location, "[:-]+", n = 3),
-    nucleotides = str_split_fixed(
-      str_split_fixed(vepfile_df$Uploaded_variation,
-        "[_-]+",
-        n = 4
-      )[, 4], "/",
-      n = 2
-    )
-  )
 
-  gene_mutation$gene_mut <- paste0(
-    gene_mutation$nucleotides.1,
-    gene_mutation$gene_mut_loc.2,
-    gene_mutation$nucleotides.2
-  )
+  locations <- vepfile_df %>%
 
-  # parsing snv and protmut location
-  locations <- data.frame(
-    gene_mut_loc = str_split_fixed(vepfile_df$Location, "[:-]+", n = 3),
-    gene_mut = gene_mutation$gene_mut,
-    prot_mut_loc = vepfile_df$Protein_position,
-    AAs = str_split_fixed(vepfile_df$Amino_acids, "/", 2),
-    Conseq = vepfile_df$Consequence,
-    genes = vepfile_df$SYMBOL
-  )
+    rename(
+      prot_pos  = Protein_position,
+      conseq    = Consequence,
+      gene_name = SYMBOL) %>%
 
-  locations <- dplyr::na_if(locations, "")
+    # get general info on mutation and its position
+    mutate(
+      mut_nucs   = str_extract(Uploaded_variation, "[A-Z]/[A-Z]$"),
+      mut_chrom  = str_extract(Location, "^[^:]+"),
+      mut_start  = str_extract(Location, "(?<=:)[0-9]+[^-]"),
+      mut_end    = str_extract(Location, "(?<=-)[0-9]+")) %>%
 
-  # delete all rows with no protein position value
-  locations <- distinct(locations %>% filter(!grepl("^-", prot_mut_loc)))
-  # specific B117 mutations: 21990-21993, 21764-21770, maybe also 3675-3677,
-  # 69-70 - all there
+    # generate unique string describing mutation
+    mutate(
+      mut_ref = str_extract(mut_nucs, "^[A-Z]"),
+      mut_var = str_extract(mut_nucs,  "[A-Z]$"),
 
-  deletions_df <- locations %>%
-    filter(gene_mut_loc.2 != gene_mut_loc.3 &
-      Conseq == "inframe_deletion" &
-      str_detect(prot_mut_loc, "-"))
+      mut_str = paste0(mut_ref, mut_start, mut_var)
+    ) %>%
 
-  colnames <- colnames(deletions_df)
+    # get infos on mutation consequences for protein
+    mutate(
+      # TODO This may behave unexpectedly; in the case of no change aa_ref
+      # and aa_var are the same, aa_var will not be empty / NA
+      aa_ref = str_extract(Amino_acids, "^[A-Z]"),
+      aa_var = str_extract(Amino_acids,  "[A-Z]$"),
 
-  # if there is a deletion the snv would span a couple of positions, if there is
-  # not such a spanning region there are no deletions
-  # ! 06/05/2021 Vic - I think, I don't know how robust this is, but it will
-  # work for the sig mutations we have so far
-  if (nrow(locations) >= 1 && !(any(is.na(locations[, "gene_mut_loc.3"])))) {
-    deletions <- dplyr::bind_rows(apply(deletions_df, 1, detectable_deletions,
-      colnames = colnames
-    ))
-    locations <- dplyr::bind_rows(locations, deletions)
-  }
+      aa_str = paste0(aa_ref, prot_pos, aa_var)
+    ) %>%
 
-  # substitute "nothing" at alternative-aa-column (for deletions) with "-"
-  locations$AAs.2[is.na(locations$AAs.2)] <- "-"
+    # delete all rows with no protein position value
+    # TODO Why do we delete these mutations? This just means they do not
+    # affect the proteins, the mutations may still be informative...
+    filter(! str_detect(prot_pos, "^-")) %>%
+    distinct() %>%
+    # TODO What is the relevance of this mutation?
+    # specific B117 mutations: 21990-21993, 21764-21770, maybe also 3675-3677,
+    # 69-70 - all there
 
-  # clean - characters
-  locations$AA_mut <- paste(locations$AAs.1,
-    locations$prot_mut_loc,
-    locations$AAs.2,
-    sep = ""
-  )
+    # remove unneeded cols
+    dplyr::select(-matches(names(vepfile_df), ignore.case = FALSE))
 
-  # adding gene information
-  locations$AA_mut <- paste(locations$genes,
-    locations$AA_mut,
-    sep = ":"
-  )
+    # TODO Previously deletions were handled by generating pseudo rows for each
+    # one. I currently do not have access to a file with deletions, and so
+    # cannot understand how to handle this properly. However this solution here
+    # is still better, because due to a bug the previous solution could almost
+    # never run. Now there is at least a warning...
+    if (any(!is.na(locations$mut_end))) {
+      warning("Found a deletion. Help.")
+    }
 
   return(locations)
 }
