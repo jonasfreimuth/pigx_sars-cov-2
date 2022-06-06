828001941191ae1e4d9dea3b7c87314bcf4c5fc9
jonasfreimuth <68993915+jonasfreimuth@users.noreply.github.com>
2022-06-06 14:17:54 +0200

    remove additional args to fread
      this makes the corresponding summary function safer as it will throw
      an error when one would supply files with mismatched dfs

--- scripts/creating_variants_summary_table.R
+++ scripts/creating_variants_summary_table.R
@@ -6,10 +6,7 @@ create_summary <- function(files) {
   # read files into list
   variants_list <- lapply(
     X = files,
-    FUN = fread,
-    header = TRUE,
-    colClasses = "character",
-    check.names = FALSE
+    FUN = fread
   )
 
   # remove empty files from list
