3efcd47c8f1726b0b3affc749b05381deaff0d5c
jonasfreimuth <68993915+jonasfreimuth@users.noreply.github.com>
2022-06-07 14:43:44 +0200

    simplify parsing_mutation_plot_data

--- scripts/pred_mutation_increase.R
+++ scripts/pred_mutation_increase.R
@@ -1,25 +1,31 @@
 library(dplyr)
 library(stringr)
 
-parsing_mutation_plot_data <- function ( mutation_plot_data ){
+parsing_mutation_plot_data <- function(mutation_plot_data) {
   #' taking csv as input which will be made by the variant reports
   #' returns df containing only mutations with frequency values
-  
+
   # TODO check file format assumptions
-  
-  #  get names of mutations (everything after the meta info from the sample sheet)
-  mutations <- names ( 
-            mutation_plot_data[ (which( names(mutation_plot_data) %in% "coordinates_long")+1) : length( names( mutation_plot_data ))])
-  
-  # enforcing date type for column dates ...it will get rid of the time
-  mutation_plot_data$dates <- as.Date ( mutation_plot_data$dates )
-  
-  # remove mutations with NA for all rows and create a new dataframe
-  # fixme Vic I think there is a more compact way for this
-  not_all_na <- function( x ) any( !is.na( x ) )
-  lm_df <- mutation_plot_data %>% select( c( dates, all_of(mutations) )) %>% select( where( not_all_na ))
-  
-  return ( lm_df )
+
+  # create vecotr of metadata col names to be excluded
+  meta_cols_excl <- c(
+    "samplename",
+    "location_name",
+    "coordinates_lat",
+    "coordinates_long"
+  )
+
+  lm_df <- mutation_plot_data %>%
+
+    # enforcing date type for column dates ...it will get rid of the time
+    mutate(dates = as.Date(dates)) %>%
+
+    select(- all_of(meta_cols_excl)) %>%
+
+    # remove mutations with NA for all rows and create a new dataframe
+    select(where(function(x) any(!is.na(x))))
+
+  return(lm_df)
 }
 
 refined_lm_model <- function( mutations.df ){
