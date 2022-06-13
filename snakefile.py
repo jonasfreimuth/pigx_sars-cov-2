ef29163dd845e37681cf5e683470455f7bc75561
jonasfreimuth <68993915+jonasfreimuth@users.noreply.github.com>
2022-06-13 16:05:58 +0200

    fix deconv logging in snakefile

--- snakefile.py
+++ snakefile.py
@@ -888,7 +888,7 @@ rule run_deconvolution:
         "{output.variant_proportions}" \
         "{output.variants_with_meta}" \
         "{output.mutations}" \
-        "{DECONVOLUTION_METHOD}"
+        "{DECONVOLUTION_METHOD}" \
         > {log} 2>&1
         """
 
