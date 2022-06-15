bd2072499792f59665cf13a286f0755988cb7e1e
jonasfreimuth <68993915+jonasfreimuth@users.noreply.github.com>
2022-06-15 18:09:39 +0200

    break up execute_deconvolution block
      done to handle case of svr deconv method not working with 2 or less
      lineages

--- scripts/deconvolution.R
+++ scripts/deconvolution.R
@@ -187,11 +187,11 @@ fwrite(
 # get  NT mutations only, input for the signature matrix
 mutations_vec <- match_df$gene_mut_collapsed
 
-# only execute the deconvolution when at least one signature mutation was found
-execute_deconvolution <- length(mutations_vec) > 0
+# only create & dedupe the signature matrix when at least one signature mutation
+# was found
+run_sigmat_dedupe <- length(mutations_vec) > 0
 
-
-if (execute_deconvolution) {
+if (run_sigmat_dedupe) {
   ## ----creating_signature_matrix, include = FALSE-----------------------------
   # create an empty data frame add a column for the Wildtype
   # Wildtype in this case means the reference version of SARS-Cov-2
@@ -252,6 +252,24 @@ if (execute_deconvolution) {
 
   ## ----calculate_sigmat_weigths, include = FALSE------------------------------
   deconv_lineages <- colnames(msig_deduped_df)
+
+  # special case: svr can't run on two or less variants, so we need to break
+  # early if that is the case
+  if (length(deconv_lineages) <= 2 && deconv_model == "svr") {
+    no_svr_exception <- FALSE
+  } else {
+    no_svr_exception <- TRUE
+  }
+}
+
+# if sigmat dedupe did not run no_svr_exception cannot be known
+if (run_sigmat_dedupe) {
+  run_deconvolution <- no_svr_exception
+} else {
+  run_deconvolution <- FALSE
+}
+
+if (run_deconvolution) {
 
   if (do_weighting) {
     # create list of proportion values that will be used as weigths
