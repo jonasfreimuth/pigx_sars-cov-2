59cb887b3737f8d2fa7cd5ec8a16dd9d83f4113f
jonasfreimuth <68993915+jonasfreimuth@users.noreply.github.com>
2022-10-06 17:55:24 +0200

    Remove separation of FTP and HTTP[S] protocols
      This should also open the function to use all URL protocols supported
      by urllib.

--- snakefile_scripts/download_tarball.py
+++ snakefile_scripts/download_tarball.py
@@ -1,5 +1,4 @@
 import re
-import requests
 import tarfile
 import sys
 import urllib.request as urlrequest
@@ -22,29 +21,13 @@ def download_tarball(dl_url):
     logger.info(
         f"Attempting to download database archive from {dl_url}...")
     
-    if re.match("ftp://*", dl_url):
-        try:
-            dl_resp = urlrequest.urlopen(dl_url)
+    try:
+        dl_resp = urlrequest.urlopen(dl_url)
 
-        except Exception as e:
-            logger.error(e)
-            sys.exit(1)
+    except Exception as e:
+        logger.error(e)
+        sys.exit(1)
 
-        tar_out = tarfile.open(fileobj = dl_resp, mode="r:gz")
-
-    elif re.match("http[s]?://*", dl_url) or not re.match("[a-z]*://", dl_url):
-        # FIXME Change http download to also use urllib.request for consistency.
-
-        dl_resp = requests.get(dl_url, stream=True)
-
-        if not dl_resp.status_code == 200:
-            raise Exception(
-                f"Download not successfull, status code {dl_resp.status_code}")
-
-        tar_out = tarfile.open(fileobj=dl_resp.raw, mode="r:gz")
-
-    else:
-        raise Exception(
-            f"Cannot resolve url protocol in {dl_url}")
+    tar_out = tarfile.open(fileobj = dl_resp, mode="r:gz")
 
     return(tar_out)
